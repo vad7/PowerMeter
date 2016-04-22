a3d8794afced1d3b6484579d30787fe71c6ccc07
Vadim <vad7@yahoo.com>
2016-04-22 16:49:01 +0300

    make_firmware_image.py

--- bin/make_firmware_image.py
+++ bin/make_firmware_image.py
@@ -7,8 +7,10 @@
 # (c) vad7
 
 import argparse
+import os
 
 argp = argparse.ArgumentParser()
+argp.add_argument('flashsize', action='store', help='Flash size, kb')
 argp.add_argument('dir', action='store', help='Directory of *.bin')
 args = argp.parse_args()
 fout_name = args.dir + "firmware.bin"
@@ -23,5 +25,14 @@ fin.close()
 data = data + data2
 fout.write(data)
 fout.flush()
+size = os.fstat(fout.fileno()).st_size
 fout.close()
 print "Make: " + fout_name
+if int(args.flashsize) == 512:
+	webfs = (size + 0xFFF) & 0xFF000
+	maxota = (0x7B000 / 2) & 0xFF000
+else:
+	webfs = 0x80000
+	maxota = 0x7B000
+print "Firmware size: " + str(size) + ", WebFS addr: " + str(webfs) + ", Max OTA size: " + str(maxota)
+print "Space available for OTA: " + str(maxota - size)
